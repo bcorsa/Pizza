28779c227887f2f818d98a38a9f12d2cefcd9ae3
stevenzim <steven_e_zimmerman@yahoo.com>
2015-02-06 15:35:33 +0000

    added pos tagger, tokenizer and tests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/903group6/Pizza',
     'author_email': 'szimme@essex.ac.uk',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose','nltk'],
     'packages': ['raop'],
     'scripts': [],
     'name': 'group6raop'
--- tests/preprocess__tests.py
+++ tests/preprocess__tests.py
@@ -1,11 +1,29 @@
 from nose.tools import *
-import NAME
+import raop.preprocess.preprocess as preprocess
 
-def setup():
-    print "SETUP!"
 
-def teardown():
-    print "TEAR DOWN!"
+testDict = {'text' : 'I am here.'}
 
-def test_basic():
-    print "I RAN!"
+def test_set_dict():
+	'''Test to load dictionary'''
+	preProcObj = preprocess.Preprocess()
+	preProcObj.setDictionary(testDict)
+	assert_equal(preProcObj.kaggleDict,testDict)
+
+
+def test_tokenize():
+	'''Test for word tokenizer'''
+	preProcObj = preprocess.Preprocess()
+	preProcObj.setDictionary(testDict)
+	preProcObj.tokenize(preProcObj.kaggleDict.get('text'))
+	assert_equal(preProcObj.tokenizedText, ['I', 'am', 'here', '.'])
+
+
+def test_pos_tag():
+	'''Test for pos tagger'''
+	preProcObj = preprocess.Preprocess()
+	preProcObj.setDictionary(testDict)
+	preProcObj.tokenize(preProcObj.kaggleDict.get('text'))
+	preProcObj.posTag(preProcObj.tokenizedText)
+	assert_equal(preProcObj.POS_TaggedText , [('I', 'PRP'), ('am', 'VBP'), ('here', 'RB'), ('.', '.')])
+
