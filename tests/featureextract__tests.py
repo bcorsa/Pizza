d1b6231c12f657570f57aeeb4b1c0b23cf985b2c
c4n <udomc.can@gmail.com>
2015-02-11 19:06:16 +0000

    deleted dummy fcns/modules

--- tests/featureextract__tests.py
+++ tests/featureextract__tests.py
@@ -1,15 +1,6 @@
 from nose.tools import *
 import raop.featureextract.featureextract as featureextract
 
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
-
 def testEvidence():
     '''Test for find evidence function'''
     testTextEvi_true = "This is the state of my refrigerator: [Linky](http://i.imgur.com/JRvYJ5s.jpg)"
